577a4f38ce1eda39ce88caabd3715157f1412c20
nazneen636 <nazneennahar636@gmail.com>
2025-03-16 15:27:13 +0600

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-orange mb-4">404</h1>
+        <p className="text-xl text-gray-700">
+          This page is under construction.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
