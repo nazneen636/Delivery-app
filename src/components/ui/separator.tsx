966dc972c9be9238b98cb72d1e39563b5ad313d3
nazneen636 <nazneennahar636@gmail.com>
2025-03-19 09:16:58 +0600

    card page responsive done

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -3,12 +3,9 @@
 import * as React from "react";
 import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { cn } from "@/lib/utils";
-
 function Separator({
   className,
   orientation = "horizontal",
-  decorative = true,
   ...props
 }: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
   return (
